2833b851a0c4c9b451123c3760bc691fbc3229ad
PoojaB26 <pbhaumik26@gmail.com>
2018-12-20 14:19:31 +0530

    banner removed

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(),
         body: NoContent()),
