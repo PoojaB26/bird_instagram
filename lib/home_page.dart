8c62573008fce86bbe39b3ca81d93c0ecc9aed01
PoojaB26 <pbhaumik26@gmail.com>
2018-12-20 15:05:13 +0530

    postlists added with dummy text

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:bird_instagram/no_content.dart';
+import 'package:bird_instagram/posts_list.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NoContent(),
+      body: PostsList(),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:bird_instagram/posts_list.dart';
 import 'package:bird_instagram/main.dart';
 
 void main() {
@@ -26,5 +26,16 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+  });
+
+
+  testWidgets('Renders list of posts', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MaterialApp(
+      home: PostsList(),
+    ));
+
+    expect(find.byType(Card), findsNWidgets(2));
+    expect(find.text('Prim Birb'), findsNWidgets(2));
   });
 }
